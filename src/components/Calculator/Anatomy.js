62d9523174d091ab739584b74e2487778c6d6902
Will Weaver <monkey@buildingbananas.com>
2018-05-22 20:27:34 -0400

    Adding reference points to table of values

--- src/components/Calculator/Anatomy.js
+++ src/components/Calculator/Anatomy.js
@@ -8,15 +8,16 @@ const styles = theme => ({});
 
 
 const AnatomyRow = props => {
-    const { label, calculated, actual } = props;
+    const { location, description, calculated} = props;
     const unit = props.unit || "cm";
 
     return (
         <tr>
-            <td>{label}</td>
+            <td>{location}</td>
+            <td>{description}</td>
             <td>{calculated}</td>
             <td>{unit}</td>
-            <td><input type="text" value={actual} size="8" /></td>
+            <td><input type="text" size="8" /></td>
             <td>{unit}</td>
         </tr>
     )
@@ -25,14 +26,15 @@ const AnatomyRow = props => {
 const Anatomy = props => {
     const { ThU } = props
     const values = Object.values(T.distanceFromSolesOfTheFeetInThU)
-    const rows = values.map(v => <AnatomyRow label={v.name} calculated={T.round2(v.distance * ThU)} />)
+    const rows = values.map(v => <AnatomyRow location={v.loc} description={v.desc} calculated={T.round2(v.distance * ThU)} />)
 
     return (
         <div>
-            <h2>Anatomical Measurements</h2>
+            <h2>Anatomical Measurements - Circle No 1 of Table I</h2>
             <table>
                 <tr>
-                    <th>Measure</th>
+                    <th>Location</th>
+                    <th>Description</th>
                     <th colSpan="2">Calculated</th>
                     <th colSpan="2">Actual</th>
                 </tr>
