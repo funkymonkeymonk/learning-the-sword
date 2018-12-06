3f6d2b1638c14af55bdf1435e40ea47328eea87e
Will Weaver <monkey@buildingbananas.com>
2018-12-06 15:02:03 -0500

    Bump

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 import Listing from './Listing';
-import SEO from './SEO';
 import Footer from './Footer';
 import Header from './Header';
 import SliceZone from './SliceZone';
@@ -7,5 +6,6 @@ import Title from './Title';
 import Layout from './Layout';
 import Wrapper from './Wrapper';
 import Calculator from './ThibaultCalculator';
+import SEO from './SEO';
 
 export { Footer, Layout, Listing, SEO, Wrapper, SliceZone, Title, Header, Calculator };
